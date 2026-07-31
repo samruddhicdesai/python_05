bbc8a06c7463da2f6022994b622ab003d2620ce0
samruddhicdesai <samruddhicdesai@gmail.com>
2026-07-31 22:53:09 +0530

    Add files via upload

---
+++ average.py
@@ -0,0 +1,4 @@
+num1 = int(input("Enter the num1 = "))
+num2 = int(input("Enter the num2 = "))
+average = (num1 + num2)/2
+print("Average of num1 and num2 = ",average)
---
+++ report.py
@@ -0,0 +1,14 @@
+name = input("Enter your name = ")
+sub1 = int(input("Enter the marks of subject 1 = "))
+sub2 = int(input("Enter the marks of subject 2 = "))
+sub3 = int(input("Enter the marks of subject 3 = "))
+sub4 = int(input("Enter the marks of subject 4 = "))
+sub5 = int(input("Enter the marks of subject 5 = "))
+total = sub1+sub2+sub3+sub4+sub5
+average = (sub1+sub2+sub3+sub4+sub5)/5
+print("-------Student Report----------")
+print("Name : ",name)
+print("Total : ",total)
+print("Average : ",average)
+
+
