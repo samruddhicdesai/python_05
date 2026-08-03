17241f5bd7de4ce84c9bc03b76bc4fd28cd45dbc
samruddhicdesai <samruddhicdesai@gmail.com>
2026-08-03 22:51:12 +0530

    Add files via upload

---
+++ allcamparison.py
@@ -0,0 +1,9 @@
+num1 = int(input("Enter the num1 = "))
+num2 = int(input("Enter the num2 = "))
+print(num1 == num2)
+print(num1 != num2)
+print(num1 > num2)
+print(num1 < num2)
+print(num1 >= num2)
+print(num1 <= num2)
+
