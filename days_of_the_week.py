85d131ad37f3a421fe9006c0953f9388d3e86f07
samruddhicdesai <samruddhicdesai@gmail.com>
2026-08-04 20:40:18 +0530

    Add files via upload

---
+++ days_of_the_week.py
@@ -0,0 +1,21 @@
+day = int(input("Enter the number = "))
+match day:
+    case 1:
+        print("Monday")
+    case 2:
+        print("Tuesday") 
+    case 3:
+        print("Weduesday")
+    case 4:
+         print("Thrusday")
+    case 5:
+        print("Friday")
+    case 6:
+         print("Saturday")
+    case 7:
+         print("Sunday")
+    case _:
+        print("INVAILD")
+    
+    
+     
---
+++ even_nums.py
@@ -0,0 +1,4 @@
+for i in range(1,101):
+    if i%2==0:
+        print("Even number =")
+        print(i)
