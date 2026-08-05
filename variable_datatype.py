28344a6cdb2a2554c204c6ba787bad27c5290ede
samruddhicdesai <samruddhicdesai@gmail.com>
2026-08-05 21:55:52 +0530

    Add files via upload

---
+++ variable_datatype.py
@@ -0,0 +1,6 @@
+name = "Samruddhi Chagan Desai"
+age = 20
+height = 5.5
+student = True
+
+print(f"My name is {name}. \nI am {age} years old.\n My height is {height}.\n I am a student and that is {student}. ")
