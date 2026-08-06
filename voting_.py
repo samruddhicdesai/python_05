993ff504d06539cf0f0e24d9618f76b0f5c2bb5c
samruddhicdesai <samruddhicdesai@gmail.com>
2026-08-06 07:20:59 +0530

    Add files via upload

---
+++ voting_.py
@@ -0,0 +1,10 @@
+age = int(input("Enter the age = "))
+if age > 18:
+    print("You are a adult")
+    print("You can drive ")
+elif age < 18:
+    print("You are just a kid")
+    print("You cannot drive")
+else:
+    print("You are just 18")
+    print("You should get permission")
