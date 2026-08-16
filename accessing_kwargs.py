d943d12117efb1607b8de143f7a4519a8a3ba759
samruddhicdesai <samruddhicdesai@gmail.com>
2026-08-16 03:44:06 -0700

    Add files via upload

---
+++ accessing_kwargs.py
@@ -0,0 +1,5 @@
+def student(**kwargs):
+    print("Name:",kwargs["name"])
+    print("Age:",kwargs["age"])
+
+student(name = "sam",age = 20)
---
+++ argus_students.py
@@ -0,0 +1,5 @@
+def student(name,*marks):
+    print("Name :",name)
+    print("Marks : ",marks)
+
+student("Sam",23,45,6,7)
